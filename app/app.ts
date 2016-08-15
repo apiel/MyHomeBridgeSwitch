dbc19119287556d4d7e172be8833c43cda5daad3
Your Name <you@example.com>
2016-08-15 10:00:31 +0000

    setup restify

--- app/app.ts
+++ app/app.ts
@@ -1,1 +1,10 @@
-console.log('hello');
+import restify = require('restify');
+
+restify.CORS.ALLOW_HEADERS.push('authorization');
+
+var server = restify.createServer();
+server.use(restify.CORS());
+
+server.listen(3030, function() {
+  console.log('%s listening at %s', server.name, server.url);
+});
