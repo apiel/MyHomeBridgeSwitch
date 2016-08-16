9210d4b611e990f502b72682c58a8a617b5314d1
Your Name <you@example.com>
2016-08-16 20:25:26 +0000

    sendCode

--- app/services/switch.service.ts
+++ app/services/switch.service.ts
@@ -14,8 +14,28 @@ export class SwitchService{
         if (_switch) {
             _switch.status = _switch.status === "on" ? "off" : "on";
             this.switchModel.save();
+            this.sendCode(_switch, _switch.status);
         }        
         
         return _switch;
+    }
+    
+    sendCode(_switch: Switch, status: string) {
+        var http = require('http');
+        http.get({
+          hostname: '192.168.0.31', // add ip in switch setting
+          path: '/send?bit=' + _switch[status].bit + '&code=' + _switch[status].code,
+          agent: false  // create a new agent just for this one request
+        }, (response) => {
+                // Do stuff with response
+              //  var str = '';
+              //  response.on('data', function (chunk) {
+              //    str += chunk;
+              //  });
+              //
+              //  response.on('end', function () {
+              //    console.log(str);
+              //  });  
+        }).end();        
     }
 }
