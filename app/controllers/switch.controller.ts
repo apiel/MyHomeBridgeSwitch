77b4c7940134916ca8418a45f53c697be8fa7a3a
Your Name <you@example.com>
2016-09-21 19:22:18 +0000

    setStatus

--- app/controllers/switch.controller.ts
+++ app/controllers/switch.controller.ts
@@ -5,35 +5,28 @@ import { Switch } from './../models/switch';
 export default class SwitchController {
     constructor(private switchService: SwitchService) {}
     
-    statusResponse(_switch: Switch, res: restify.Response, next: restify.Next) {
-        if (_switch) {
-            res.json(200, {status: _switch.status});
-        }
-        else {
-            res.json(200, {error: "unknown key"});
-        }
-        return next();        
-    }
-    
     status(req: restify.Request, res: restify.Response, next: restify.Next) {
         let id = req.params['id'];
-        let _switch: Switch = this.switchService.get(id);
-        
-        return this.statusResponse(_switch, res, next);
-    }
-    
-//    toggle(req: restify.Request, res: restify.Response, next: restify.Next) {
-//        let id = req.params['id'];
-//        let _switch: Switch = this.switchService.toggle(id);
-//        
-//        return this.statusResponse(_switch, res, next);
-//    }   
+        try {
+            let _switch: Switch = this.switchService.get(id);            
+            res.json(200, {status: _switch.status});
+        }
+        catch(e) {
+            res.json(200, {error: e});
+        }
+        return next();
+    } 
     
     setStatus(req: restify.Request, res: restify.Response, next: restify.Next) {
         let id = req.params['id'];
         let status = req.params['status'];
-        let _switch: Switch = this.switchService.setStatus(id, status);
-        
-        return this.statusResponse(_switch, res, next);
+        try {
+            let _switch: Switch = this.switchService.setStatus(id, status);            
+            res.json(200, {status: _switch.status});
+        }
+        catch(e) {
+            res.json(200, {error: e});
+        }
+        return next();
     } 
 }
--- app/services/switch.service.ts
+++ app/services/switch.service.ts
@@ -7,29 +7,11 @@ export class SwitchService{
     
     get(id: number): Switch {
         let _switch: Switch = this.switchModel.get(id);
+         if (!_switch) {
+            throw "Unknown switch key";
+        }
         return _switch;
     }
-    
-//    toggle(id: number): Switch {
-//        let _switch: Switch = this.get(id);
-//        if (_switch) {
-//            for(let urlKey in _switch.urls) {
-//                if (!_switch.status) {
-//                    _switch.status = urlKey;
-//                    break;
-//                }
-//                else if (_switch.status === urlKey) {
-//                    _switch.status = null;
-//                }
-//            }
-//            if (!_switch.status) {
-//                _switch.status = Object.keys(_switch.urls)[0];
-//            }
-//            _switch = this.setStatus(id, _switch.status);           
-//        }        
-//        
-//        return _switch;
-//    }
     
     toggle(_switch: Switch): string {
         for(let urlKey in _switch.urls) {
@@ -49,19 +31,19 @@ export class SwitchService{
     
     setStatus(id: number, status: string): Switch {
         let _switch: Switch = this.get(id);
-        if (_switch) {
-            if (_switch.urls[status]) {
-                _switch.status = status;
-            }
-            else if (status === "toggle") {
-                _switch.status = this.toggle(_switch);
-            }
-            else {
-                // throw error status does not exist
-            }
-            this.switchModel.save();
-            request(_switch.urls[_switch.status]);                  
-        }        
+
+        if (_switch.urls[status]) {
+            _switch.status = status;
+        }
+        else if (status === "toggle") {
+            _switch.status = this.toggle(_switch);
+        }
+        else {
+            throw "Status does not exist";
+        }
+        this.switchModel.save();
+        request(_switch.urls[_switch.status]);                  
+ 
         return _switch;
     }    
 }
